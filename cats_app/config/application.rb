4f277bb0fe9121ccd949bdb8eec2888e03d0df57
yuris <yls4@cornell.edu>
2023-09-29 14:48:21 -0400

    initial commit

--- cats_app/config/application.rb
+++ cats_app/config/application.rb
@@ -22,6 +22,7 @@ module CatsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.action_controller.default_protect_from_forgery = false
 
     # Configuration for the application, engines, and railties goes here.
     #
