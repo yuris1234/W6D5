b5762998fe8e699b656f1299b7dddf59858086a8
yuris <yls4@cornell.edu>
2023-09-29 16:48:21 -0400

    seeded db

--- cats_app/db/seeds.rb
+++ cats_app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do
+    # Destroy tables (not necessary if using rails db:seed:replant)
+    puts 'Destroying tables...'
+    Cat.destroy_all
+
+    puts 'Resetting id sequences...'
+    %w(cats).each do |table_name|
+        ApplicationRecord.connection.reset_pk_sequence!(table_name)
+    end
+
+    puts 'Seeding database...'
+    Cat.create!(name: 'Yuri', birth_date: Date.new(2001,2,1), color: "Red", sex: "F")
+    Cat.create!(name: 'Jacob', birth_date: Date.new(2000,3,1), color: "Blue", sex: "M", description: "Sick")
+    puts "Done!"
+end
